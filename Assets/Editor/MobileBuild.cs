08f4c15cd952303468048e98b2e32ea5d8235402
SASE <sase1016@gmail.com>
2020-07-01 15:46:13 +0900

    up date

---
+++ Assets/Editor/MobileBuild.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+
+public class MobileBuild
+{
+    public static void TestBuild()
+    {
+        // ビルド対象シーンリスト
+        string[] sceneList = {
+            "./Assets/Scenes/SampleScene.unity",
+        };
+
+
+        // 実行
+        BuildReport errorMessage = BuildPipeline.BuildPlayer(
+            sceneList,                          //!< ビルド対象シーンリスト
+            "C:/project/bin/GitHubActionsBuild.apk",   //!< 出力先
+            BuildTarget.Android,      //!< ビルド対象プラットフォーム
+            BuildOptions.Development            //!< ビルドオプション
+        );
+        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+
+        // 結果出力
+        if( !string.IsNullOrEmpty( errorMessage.ToString() ) )
+            Debug.LogError( "[Error!] " + errorMessage );
+        else
+            Debug.Log( "[Success!]" );
+    }
+    
+
+}
