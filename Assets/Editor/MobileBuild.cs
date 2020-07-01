828f90353dde233366b860cd31910bfff890f48f
SASE <sase1016@gmail.com>
2020-07-01 16:51:43 +0900

    up date2

--- Assets/Editor/MobileBuild.cs
+++ Assets/Editor/MobileBuild.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor.Callbacks;
 
 public class MobileBuild
@@ -31,6 +32,29 @@ public class MobileBuild
         else
             Debug.Log( "[Success!]" );
     }
+
+
     
+    [MenuItem("Build/BuildApk")]
+    public static void BuildApk()
+    {
+        var outdir = System.Environment.CurrentDirectory + "/BuildOutPutPath/Android";
+        var outputPath = Path.Combine(outdir, Application.productName + ".apk");
+        //文件夹处理
+        if (!Directory.Exists(outdir)) Directory.CreateDirectory(outdir);
+        if (File.Exists(outputPath)) File.Delete(outputPath);
+
+        //开始项目一键打包
+        string[] scenes = new[] {"Assets/Scenes/SampleScene.unity"};
+        UnityEditor.BuildPipeline.BuildPlayer(scenes, outputPath, BuildTarget.Android, BuildOptions.None);
+        if (File.Exists(outputPath))
+        {
+            Debug.Log("Build Success :" + outputPath);
+        }
+        else
+        {
+            Debug.LogException(new Exception("Build Fail! Please Check the log! "));
+        }
+    }
 
 }
